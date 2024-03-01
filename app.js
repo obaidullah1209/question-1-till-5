8d5d0b048a5a32ae72148f6f2494c9fe61fcfa6a
obaidullah1209 <mobaidullah554@gmail.com>
2024-02-29 19:14:36 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// question1
+alert("Welcome to our website. We're delighted to have you here")
+// question2
+alert("Error! Please enter a valid password")
+// question3
+alert("Welcome to JS Land.. \n Happy Coding!")
+// question4
+alert("Welcome to JS Land..")
+alert("Happy Coding")
+// question5
+console.log("hello I can run JS through my website developer console");
